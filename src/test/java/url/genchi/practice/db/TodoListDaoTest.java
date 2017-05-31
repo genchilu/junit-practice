4f052ee7abd717cb0cfc65c29640dce60a735e35
genchi.lu[盧俊錡] <genchi.lu@104.com.tw>
2017-05-31 11:55:29 +0800

    Add unittest
    1. test find exist id
    2. test find non exist id
    3. test find all
    4. test delete todolist

--- src/test/java/url/genchi/practice/db/TodoListDaoTest.java
+++ src/test/java/url/genchi/practice/db/TodoListDaoTest.java
@@ -4,7 +4,11 @@ import static org.junit.Assert.assertTrue;
 import java.io.InputStream;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
+import java.util.UUID;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -42,7 +46,7 @@ public class TodoListDaoTest extends TestCase{
   }
 
   @Test
-  public void testSaveTodoList() throws SQLException {
+  public void testSaveTodoList() throws Exception {
     TodoList tdlOri = new TodoList("test save");
     this.tdo.saveTodoList(tdlOri);
     TodoList tdlFromDao = tdo.findOneById(tdlOri.getId().toString());
@@ -50,14 +54,55 @@ public class TodoListDaoTest extends TestCase{
   }
 
   @Test
-  public void testUpdateTodoList() throws SQLException {
+  public void testUpdateTodoList() throws Exception {
     this.testTdl1.setDescription("after update");
     this.tdo.updateTodolist(testTdl1);
     TodoList tdlFromDao = tdo.findOneById(testTdl1.getId().toString());
     assertTrue(this.testTdl1.equals(testTdl1));
   }
 
+  @Test
+  public void testFindOneById_ExistId() throws Exception {
+    TodoList tdlFromDao = this.tdo.findOneById(testTdl1.getId().toString());
+    assertTrue(this.testTdl1.equals(tdlFromDao));
+  }
 
+  @Test
+  public void testFindOneById_NonExistId_ShouldNull() throws Exception {
+    TodoList tdlFromDao = this.tdo.findOneById(UUID.randomUUID().toString());
+    assertNull(tdlFromDao);
+  }
+
+  @Test
+  public void testFindAll() throws Exception {
+    List<TodoList> tdlListFromDao = this.tdo.findAll();
+    List<TodoList> testTdlList = new LinkedList<>();
+    testTdlList.add(testTdl1);
+    testTdlList.add(testTdl2);
+    assertTrue(isListEqual(testTdlList, tdlListFromDao));
+  }
+
+  static private boolean isListEqual(List<TodoList> list1, List<TodoList> list2){
+    if(list1.size() != list2.size()) {
+      return false;
+    }
+    for(int i = 0;i<list1.size();i++) {
+      boolean isIdEqual = list1.get(i).getId().toString().equals(list2.get(i).getId().toString());
+      boolean isDescEqual = list1.get(i).getDescription().equals(list2.get(i).getDescription());
+      if(!isDescEqual || !isDescEqual) {
+        return false;
+      }
+    }
+    return true;
+  }
+  @Test
+  public void testdeleteTodoList_Delete_ExistId_And_Find_Should_Null() throws Exception {
+    String id = testTdl1.getId().toString();
+    this.tdo.deleteTodoList(testTdl1);
+    TodoList tdlFromDao = this.tdo.findOneById(id);
+    assertNull(tdlFromDao);
+  }
+  
   @Override
   protected void tearDown() throws Exception {
     Statement stmt = jdbcSource.getConn().createStatement();
@@ -65,5 +110,4 @@ public class TodoListDaoTest extends TestCase{
     stmt.close();
     jdbcSource.getConn().commit();
   }
-
 }
